40b37057e9f661cd5bc3b79bb37d59f845e5318b
Paul <pacalabre@gmail.com>
2016-05-30 17:45:47 -0700

    making changes before tweaks

--- index.js
+++ index.js
@@ -21,20 +21,17 @@ app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
-
-// app.get("/greet/:name/:lastname", function(req, res) {
-//   res.send("Hello " + req.params.name + " " + req.params.lastname);
-// });
-
-// app.get("/multiply/:x/:y", function(req, res) {
-//   res.send("The answer is: " + (req.params.x * req.params.y));
-// });
-
-// app.get("/add/:x/:y", function(req, res) {
-//   res.send("The answer is: " + (parseInt(req.params.x) + parseInt(req.params.y)));
-// });
-
-
 app.listen(3000);
 
 
+// things to add:
+// - OAuth
+// - Login
+// - All Things Mongo / Database
+//   - Add / Edit Food
+// - Cloudinary Images
+// - API
+
+// nice to have:
+// - comments
+// - tags
